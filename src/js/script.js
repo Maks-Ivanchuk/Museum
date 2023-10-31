bb94f3890a8f003649672321911fd4d035277836
Maks Ivanchuk <maksivanchu@gmail.com>
2023-10-31 16:59:44 +0200

    fix problem with bg menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
    const burgerMenuBtn = document.querySelector('.header__bg-menu');
    const headerNav = document.querySelector('.header__nav');
+   const maxWidth = 1024;
+
+   function closeBurgerMenu() {
+      let curentWindowsWidth = document.documentElement.clientWidth;
+
+      if (curentWindowsWidth > maxWidth) {
+         if (headerNav.classList.contains('header__nav--open')) {
+            headerNav.classList.remove('header__nav--open');
+            burgerMenuBtn.classList.remove('header__bg-menu--open');
+         }
+      }
+   }
 
    burgerMenuBtn.addEventListener('click', () => {
-      headerNav.classList.toggle('header__nav--active');
-      let curentWidth = document.documentElement.clientWidth;
-      if (curentWidth > 1024) {
-         if (headerNav.classList.contains('header__nav--active')) {
-            console.log(curentWidth);
-         };
-      };
-   })
+      headerNav.classList.toggle('header__nav--open');
+      burgerMenuBtn.classList.toggle('header__bg-menu--open');
+   });
+
+   window.addEventListener('resize', closeBurgerMenu);
 });
