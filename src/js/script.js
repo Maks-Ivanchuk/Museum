775cb00327f50d792471604f784244e2108fc1a1
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-11-03 21:43:06 +0200

    fix section visityng

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
    const burgerMenuBtn = document.querySelector('.header__bg-menu');
    const headerNav = document.querySelector('.header__nav');
+   const body = document.querySelector('body');
+   const overlay = document.querySelector('.overlay');
    const maxWidth = 1024;
 
    function closeBurgerMenu() {
@@ -10,6 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
          if (headerNav.classList.contains('header__nav--open')) {
             headerNav.classList.remove('header__nav--open');
             burgerMenuBtn.classList.remove('header__bg-menu--open');
+            body.classList.remove('body--block');
+            overlay.classList.remove('overlay--open');
          }
       }
    }
@@ -17,7 +21,20 @@ document.addEventListener("DOMContentLoaded", () => {
    burgerMenuBtn.addEventListener('click', () => {
       headerNav.classList.toggle('header__nav--open');
       burgerMenuBtn.classList.toggle('header__bg-menu--open');
+      body.classList.toggle('body--block');
+      overlay.classList.toggle('overlay--open');
+
+      // if (document.documentElement.clientWidth == maxWidth) {
+
+      // }
    });
+
+   overlay.addEventListener('click', function () {
+      headerNav.classList.toggle('header__nav--open');
+      burgerMenuBtn.classList.toggle('header__bg-menu--open');
+      body.classList.toggle('body--block');
+      overlay.classList.toggle('overlay--open');
+   })
 
    window.addEventListener('resize', closeBurgerMenu);
 });
