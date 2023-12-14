499d862dd08280f174c6e390da3baba171051ec5
Maks Ivanchuk <maksivanchu@gmail.com>
2023-12-14 15:50:59 +0200

    save changes

--- src/js/script.js
+++ src/js/script.js
@@ -489,12 +489,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
    quantityTickets();
 
-   console.log(btnSubmit);
-
    btnSubmit.addEventListener('click', submitFormBuyTickets);
 
    /////////you tube//////////
 
+   let videoSliderItems = document.querySelector('.abc');
+   let player;
+
+   function onYouTubeIframeAPIReady() {
+      player = new YT.Player('playerSliderYT1', {
+         // height: '360',
+         // width: '640',
+         videoId: 'zp1BXPX8jcU',
+         events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+         }
+      });
+   }
+
+   window.onYouTubeIframeAPIReady = function () {
+      // Викликаємо onYouTubeIframeAPIReady при завантаженні сторінки
+      onYouTubeIframeAPIReady();
+   }
+
+   // 4. The API will call this function when the video player is ready.
+   function onPlayerReady(event) {
+      // event.target.playVideo();
+
+   }
+
+   // 5. The API calls this function when the player's state changes.
+   //    The function indicates that when playing a video (state=1),
+   //    the player should play for six seconds and then stop.
+   let done = false;
+   function onPlayerStateChange(event) {
+      if (event.data == YT.PlayerState.PLAYING && !done) {
+         // setTimeout(stopVideo, 6000);
+         // done = true;
+
+      }
+   }
+   function stopVideo() {
+      player.stopVideo();
+   }
 
    ///////////////////////////
 });
